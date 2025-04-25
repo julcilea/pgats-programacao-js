d899b2c7072f73d782f7567d78868d46e7b2f358
julcilea <julcilea@gmail.com>
2025-04-25 19:50:09 -0300

    incrementando validação

--- 02-exercicio.js
+++ 02-exercicio.js
@@ -1,13 +1,13 @@
- var dogsName = ['     Thor ', 'hela', 'rEx', 'branquinhA ', '  Tunico cheroso']
+ var dogsName = ['     Thor ', 'heLa', 'rEx@', 'branquinhA ', '  Tunico cheroso']
 
 for (let index = 0; index < dogsName.length; index++) {
     const element = dogsName[index].trim();
 
     console.log(`\n====== Validação Nome do Dog =======`)
-    console.log(`Nome original: ${element}`)
+    console.log(`Nome original: ${dogsName[index]}`)
 
     if(element.split(" ").length < 2){
-        console.log(`Nome formatado: ${element.charAt(0).toUpperCase() + element.slice(1).toLowerCase()}`)
+        console.log(`Nome formatado: ${element.charAt(0).toUpperCase() + element.slice(1).toLowerCase().replace(/\s+/g, "").replace(/[^a-zA-ZÀ-ÿ]/g, "")}`)
     } else {
         console.log(`Nome formatado: Nome inválido`)
     }   
