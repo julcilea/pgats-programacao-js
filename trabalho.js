a2a3472556426a8816ceb1db4530db7368a1db7b
julcilea <julcilea@gmail.com>
2025-05-10 21:41:22 -0300

    gtrabalho final

--- trabalho.js
+++ trabalho.js
@@ -14,16 +14,12 @@ export function geradorDeTagsDeIdentificacao(nome) {
 // 2. Verifica se o pet pode ser adotado com base na idade e no porte
 export function verificarSePodeSerAdotado(idade, porte) {
   const idadeMinima = 2
-  let statusAdocao = idade <= idadeMinima || porte === 'P' ? true : false
-  return statusAdocao
+  return idade <= idadeMinima || porte === 'P' ? true : false
 }
 
 // 3. Calcula o consumo de ração com base no peso
 export function calcularConsumoDeRacao(nome, idade, peso) {
-    const estoque = 1892250
-    let gramasPorDia = peso * 30
-    let duracaoEstoque = Math.floor(estoque / gramasPorDia)
-    return duracaoEstoque
+    return peso * 300
 }
 
 // 4. Decide a atividade com base no porte do animal
